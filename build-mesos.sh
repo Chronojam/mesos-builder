4b21b794b2dbd77f823481cadc826de332e4e9cc
Chronojam <Chronojam@Chronojam.co.uk>
2016-01-16 14:32:40 +0000

    initial commit

---
+++ build-mesos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /usr/local/src
+wget http://mirror.vorboss.net/apache/mesos/0.26.0/mesos-0.26.0.tar.gz
+tar -zxf mesos-0.26.0.tar.gz
+mkdir -p /usr/local/mesos-0.26.0/build
+cd /usr/local/src/mesos-0.26.0/build
+../configure --disable-java
+make -j 4 V=0
